22b060ed23a478aaaafded7bbc6ba15f95a70f80
Dr.Death <Dr.Death@lpi-clan.de>
2014-06-25 17:30:14 +0200

    RC1 readiness - Removed ext.php - it's not necessary anymore

--- migrations/v10x/release_1_0_0.php
+++ migrations/v10x/release_1_0_0.php
@@ -18,7 +18,7 @@ class release_1_0_0 extends \phpbb\db\migration\migration
 
 	static public function depends_on()
 	{
-		return array('\phpbb\db\migration\data\v310\beta3');
+		return array('\phpbb\db\migration\data\v310\rc1');
 	}
 
 	public function update_data()

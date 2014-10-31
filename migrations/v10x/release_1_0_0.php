ea491dfbe869af5bf5f508169f083e07f83bdbe6
Dr.Death <Dr.Death@lpi-clan.de>
2014-10-31 09:39:08 +0100

    Ready for phpBB 3.1 (gold)

--- migrations/v10x/release_1_0_0.php
+++ migrations/v10x/release_1_0_0.php
@@ -18,7 +18,7 @@ class release_1_0_0 extends \phpbb\db\migration\migration
 
 	static public function depends_on()
 	{
-		return array('\phpbb\db\migration\data\v310\rc2');
+		return array('\phpbb\db\migration\data\v310\gold');
 	}
 
 	public function update_data()
